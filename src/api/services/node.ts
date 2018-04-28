a9d7bb9b99470a6f38aaf9f22531a6ccf92855ee
Jan Bevers <jan.bevers@kunstmaan.be>
2018-04-28 10:28:48 +0200

    Node service

--- src/api/services/node.ts
+++ src/api/services/node.ts
@@ -5,17 +5,31 @@ interface IConfig {
 export default class NodeService {
     private config: IConfig
 
-    constructor({baseUrl: string}) {
-
+    constructor(config: IConfig) {
+        this.config = config
     }
 
-    public async getNodes() {
-        const {baseUrl} = this.config
-        return await fetch(`${baseUrl}/api/nodes`)
+    public async getNodes(): Promise<Definitions.INodeList> {
+        try {
+            const {baseUrl} = this.config
+            const response = await fetch(`${baseUrl}/api/nodes`)
+            const nodes = await response.json()
+            return nodes as Definitions.INodeList
+        } catch (err) {
+            console.error(err)
+            throw err
+        }
     }
 
-    public async getNode(nodeId: string) {
-        const {baseUrl} = this.config
-        return await fetch(`${baseUrl}/api/nodes/${encodeURIComponent(nodeId)}`)
+    public async getNode(nodeId: string): Promise<Definitions.IGetNode> {
+        try {
+            const {baseUrl} = this.config
+            const response = await fetch(`${baseUrl}/api/nodes/${encodeURIComponent(nodeId)}`)
+            const nodes = await response.json()
+            return nodes as Definitions.IGetNode
+        } catch (err) {
+            console.error(err)
+            throw err
+        }
     }
 }
